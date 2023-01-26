95eeae669d4d8ab7fccc13fd3c7a5e74d5b3d9c9
tinkoir <1725142@mail.ru>
2023-01-26 16:57:25 +0300

    Initial commit

---
+++ src/main/java/calc/neoflex/demo/Controller/Controller/MathController.java
@@ -0,0 +1,17 @@
+package calc.neoflex.demo.Controller.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+    @GetMapping("/sum/a={a}/{b}")
+    public Integer sumOfTwo(@PathVariable Integer a, @PathVariable Integer b) {
+        return a + b;
+    }
+    @GetMapping("sub/{a}/{b}")
+    public Integer subOfTwo(@PathVariable Integer a, @PathVariable Integer b) {
+        return a - b;
+    }
+}
